99f8ebb18b4f62037c2183e43f7f3536ba2d8dd1
Gaston Iglesias <giglesias@sml.com.ar>
2020-09-21 20:37:19 -0300

    Post

---
+++ app/src/main/java/com/ort/myapplication/Interface/APIService.java
@@ -0,0 +1,22 @@
+package com.ort.myapplication.Interface;
+
+import com.ort.myapplication.Model.Token;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface APIService {
+
+    @POST("/posts")
+    @FormUrlEncoded
+    Call<Token> savePost(@Field("title") String title,
+                        @Field("body") String body,
+                        @Field("userId") long userId);
+}
