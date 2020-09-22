644a958f4e78b1a95e290b20ce1d0c201c16bee3
Pablo.Andres <pablojorgeandres@gmail.com>
2020-09-22 14:48:15 -0300

    Main Activity changed by Login

--- app/src/main/java/com/ort/myapplication/LoginActivity.java
+++ app/src/main/java/com/ort/myapplication/LoginActivity.java
@@ -2,13 +2,31 @@ package com.ort.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button logButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        logButton = findViewById(R.id.btn_login);
+        logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                accessMainApp();
+            }
+        });
+    }
+
+    private void accessMainApp() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
