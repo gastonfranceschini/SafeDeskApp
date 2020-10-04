4d440a3fcad51fa06586fc32ee008edb063a8c9a
AlanEzeq <57920213+AlanEzeq@users.noreply.github.com>
2020-10-04 19:57:13 -0300

    Listas inicial andando

--- app/src/main/java/com/ort/myapplication/MisReservasActivity.java
+++ app/src/main/java/com/ort/myapplication/MisReservasActivity.java
@@ -6,15 +6,19 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MisReservasActivity extends AppCompatActivity {
-    private ListView mListView;
-    private ArrayAdapter aAdapter;
-    private String[] users = { "Suresh Dasari", "Rohini Alavala", "Trishika Dasari", "Praveen Alavala", "Madav Sai", "Hamsika Yemineni"};
+    private ListView mListView1, mListView2;
+    private ArrayAdapter aAdapter1, aAdapter2;
+    private String[] users1 = { "Suresh Dasari", "Rohini Alavala", "Trishika Dasari", "Praveen Alavala", "Madav Sai", "Hamsika Yemineni"};
+    private String[] users2 = { "Maria Jose", "Alan Muskat", "Ariel Sapir", "Praveen Alavala", "Madav Sai", "Hamsika Yemineni"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_misreservas);
-        mListView = (ListView) findViewById(R.id.userlist);
-        aAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, users);
-        mListView.setAdapter(aAdapter);
+        setContentView(R.layout.activity_misreservas_new);
+        mListView1 = (ListView) findViewById(R.id.userlist1);
+        aAdapter1 = new ArrayAdapter(this, android.R.layout.simple_list_item_1, users1);
+        mListView1.setAdapter(aAdapter1);
+        mListView2 = (ListView) findViewById(R.id.userlist2);
+        aAdapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1, users2);
+        mListView2.setAdapter(aAdapter2);
     }
 }
