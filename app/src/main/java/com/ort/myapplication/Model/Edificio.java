9504375cf8540a8b1a9bca102be6385ba93271d6
Rslullitel <rslullitel@prokarma.com>
2020-10-11 20:50:33 -0300

    endpoints

--- app/src/main/java/com/ort/myapplication/Model/Edificio.java
+++ app/src/main/java/com/ort/myapplication/Model/Edificio.java
@@ -1,23 +1,24 @@
 package com.ort.myapplication.Model;
 
 public class Edificio {
+
     private int id;
     private String nombre;
     private String direccion;
-    private String lat;
-    private String longitud;
+    /*private String lat;
+    private String longitud;*/
 
     public String getDireccion() {
         return direccion;
     }
 
-    public String getLat() {
+    /*public String getLat() {
         return lat;
     }
 
     public String getLongitud() {
         return longitud;
-    }
+    }*/
 
     public int getId() {
         return id;
