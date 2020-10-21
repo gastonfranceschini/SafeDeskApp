868e503316f11a831332c5336e316264bc1da515
Rslullitel <rslullitel@prokarma.com>
2020-10-21 17:59:59 -0300

    showCupo

--- app/src/main/java/com/ort/SafeDesk/Model/Edificio.java
+++ app/src/main/java/com/ort/SafeDesk/Model/Edificio.java
@@ -5,12 +5,15 @@ public class Edificio {
     private int eID;
     private String Nombre;
     private String Direccion;
+    private int Cupo;
     /*private String lat;
     private String longitud;*/
 
     public String getDireccion() {
         return Direccion;
     }
+
+    public int getCupo() { return Cupo; }
 
     /*public String getLat() {
         return lat;
--- app/src/main/java/com/ort/SafeDesk/Model/Hora.java
+++ app/src/main/java/com/ort/SafeDesk/Model/Hora.java
@@ -3,6 +3,9 @@ package com.ort.SafeDesk.Model;
 public class Hora {
     private int id;
     private String horario;
+    private int Cupo;
+
+    public int getCupo() { return Cupo; }
 
     public String getHora() {
         return horario;
