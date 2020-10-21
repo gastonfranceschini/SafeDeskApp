055fc9fec6e8243694005c7da60d3df117f2d582
Gaston Iglesias <giglesias@sml.com.ar>
2020-10-21 19:50:27 -0300

    nidea

--- app/src/main/java/com/ort/SafeDesk/utils/ApiUtils.java
+++ app/src/main/java/com/ort/SafeDesk/utils/ApiUtils.java
@@ -9,7 +9,7 @@ public class ApiUtils {
     //"http://35.190.67.223";
     //"http://10.0.2.2:3000/api/reportes/test";
 
-    public static final String BASE_URL =   "https://safedesk.apiexperta.com.ar";
+    public static final String BASE_URL =  "https://safedesk.apiexperta.com.ar";
 
     public static Object getAPI(Class IClass) {
         return RetrofitClient.getClient(BASE_URL).create(IClass);
